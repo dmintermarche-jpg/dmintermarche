5d83d97ffb93ea9af390cfc34f42bb18d79250c9
dmintermarche-jpg <dmintermarche@gmail.com>
2026-09-20 12:14:54 +0200

    Update MainActivity.kt

--- app/src/main/java/com/DMIntermarche/MainActivity.kt
+++ app/src/main/java/com/DMIntermarche/MainActivity.kt
@@ -1,47 +1,42 @@
 package com.DMIntermarche
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.DMIntermarche.ui.theme.DMInterMarcheTheme
+import androidx.compose.ui.viewinterop.AndroidView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DMInterMarcheTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                WebsiteView(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
+@SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun WebsiteView(modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl("https://dmintermarche.netlify.app/")
+            }
+        }
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DMInterMarcheTheme {
-        Greeting("Android")
-    }
 }
